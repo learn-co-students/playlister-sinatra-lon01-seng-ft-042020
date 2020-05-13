7e981e8e8c77e61def1d5ec6a99d1fe4a5474dc9
github username <github email address>
2020-05-13 13:01:54 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,17 @@
+require "pry"
 class Artist < ActiveRecord::Base
     has_many :songs
     has_many :genres, through: :songs
+
+    #Instance method - Can slugify its name
+    def slug
+        self.name.downcase.gsub(' ', '-')
+    end
+
+    #Class method - given the slug can find an Artist
+    def self.find_by_slug(slug)
+        Artist.all.find {|artist| artist.slug == slug}
+    end
 end
 
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,13 @@ class Song < ActiveRecord::Base
     has_many :song_genres
     has_many :genres,  through: :song_genres
 
+    #Instance method - Can slugify its name
+    def slug
+        self.name.downcase.gsub(' ', '-')
+    end
+
+    #Class method - given the slug can find a Song
+    def self.find_by_slug(slug)
+        Song.all.find {|song| song.slug == slug}
+    end
 end
